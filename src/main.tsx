53cc7a69e9cac770dd6e53c3e5e40d49634a1efb
Tung <tunglee0305@gmail.com>
2025-03-23 18:50:28 +0700

    fix: import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import '../public/computer_system_data/styles.css'
+import '/public/computer_system_data/styles.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
